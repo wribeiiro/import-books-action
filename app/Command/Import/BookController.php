11393333f633ec0e96c1facb9e5a2eeef73499ec
Wellisson Ribeiro <welleh10@gmail.com>
2022-08-01 13:30:54 -0300

    new test

--- app/Command/Import/BookController.php
+++ app/Command/Import/BookController.php
@@ -7,36 +7,41 @@ use Minicli\Command\CommandController;
 
 class BookController extends CommandController
 {
-    public string $API_URL = 'https://gist.githubusercontent.com/wribeiiro/304a9c10d3bcb6ffbf4da923d02dbb2d/raw/3580133f6402eece524e79531bf3c72e318750a9/books.json';
+    private string $apiURL = 'https://gist.githubusercontent.com/wribeiiro/304a9c10d3bcb6ffbf4da923d02dbb2d/raw/3580133f6402eece524e79531bf3c72e318750a9/books.json';
+    //private string $readmeFileUrl = 'https://raw.githubusercontent.com/wribeiiro/wribeiiro/master/README.MD';
 
     public function handle(): void
     {
         $this->getPrinter()->display('Fetching books from gist...');
 
         $crawler = new Client();
-        $response = $crawler->get($this->API_URL);
+        $response = $crawler->get($this->apiURL);
 
         if ($response['code'] !== 200) {
             $this->getPrinter()->error('Error while contacting the gist :/');
             return;
         }
 
-        if (!$this->getApp()->config->data_path) {
-            $this->getPrinter()->error('You must define your data_path config value.');
-            return;
-        }
-
-        $pathData = $this->getApp()->config->data_path;
         $books = json_decode($response['body']);
+        //$responseReadme = file_get_contents($this->readmeFileUrl);
 
+        //if (!$responseReadme) {
+        //    $this->getPrinter()->error('Error while contacting the readmeFileUrl :/');
+        //    return;
+        //}
+
+        $strNewContent = "";
         foreach($books->data as $book) {
-            $filepath = $pathData . '/books.json';
-            $file = fopen($filepath, 'w+');
-            fwrite($file, $book->title);
-            fclose($file);
-
-            $this->getPrinter()->info("Saved book: " . $book->title);
+            $strNewContent .= "- " . $book->title . PHP_EOL;
+            $this->getPrinter()->info("Reading book: " . $book->title);
         }
+
+        if (strlen($strNewContent) > 0) {
+            $file = fopen(dirname(__DIR__, 3) . '/README.MD', 'a');
+            fwrite($file, $strNewContent);
+            fclose($file);
+        }
+
 
         $this->getPrinter()->info("Finished importing.", true);
     }
--- config.php
+++ config.php
@@ -2,6 +2,5 @@
 
 return [
     'app_path' => __DIR__ . '/app/Command',
-    'debug' => true,
-    'data_path' => __DIR__ . '/data'
+    'debug' => true
 ];
